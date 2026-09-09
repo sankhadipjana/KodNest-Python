4d8dcedf1eb3a0034502178e3eaddfc3d3215a0a
sankhadipjana <sankhadipjana31@gmail.com>
2026-09-09 12:12:39 +0530

    second commit

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_program.py
@@ -0,0 +1,3 @@
+# print("my first program ")
+
+print("During this one-week Python journey, I learned a lot of things, \nincluding how Python works and how to write Python code properly. \nDuring this time, I also learned that Python is very flexible and can be used to learn and build things in multiple fields, \nsuch as web development, machine learning, and data science. \nThat’s why I like Python so much. This is my first Python code in AntiGravity, \nand I’m excited to continue learning more.")
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/pseudocode_2.py
@@ -0,0 +1,20 @@
+import numbers
+number  = int ( input("any number"))
+
+# positive negative
+if number == 0:
+    print("zero")
+elif number > 0:
+    print ("positive")   
+else:
+    print("negative")    
+
+#odd even 
+if number%2 == 0:
+    print ("even number")
+else:
+    print("odd")    
+    
+  
+
+
